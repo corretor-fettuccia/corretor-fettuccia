673fc7f3318b19e55e7f66376052c398351e5ff1
corretor-fettuccia <corretor.fettuccia@gmail.com>
2025-08-29 21:34:50 -0300

    Add files via upload

--- tour360/view360.js
+++ tour360/view360.js
@@ -158,12 +158,6 @@ const urlParams = new URLSearchParams(window.location.search);
   // Atualizar visibilidade dos botões
   setInterval(updateNavButtons, 100);
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-58D75TVH');
-
 
 
 
